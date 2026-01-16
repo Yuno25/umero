19f651fbe7c81a39e50287ff8519237f176e80c0
yuno <mshrivastava051@gmail.com>
2026-01-16 19:19:57 +0530

    fix: assert MONGODB_URI for Vercel build

--- umero-landing/lib/db.ts
+++ umero-landing/lib/db.ts
@@ -1,22 +1,29 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI not defined");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = (global as any).mongoose;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// @ts-ignore
+let cached: MongooseCache = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  // @ts-ignore
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
