cc5c5aacb33fdcee0d7e54e2bda5fa7ba5a683bc
yuno <mshrivastava051@gmail.com>
2026-02-01 16:47:56 +0530

    Fix WelcomeCard import path for Vercel

--- umero-landing/components/HomeClient.tsx
+++ umero-landing/components/HomeClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import WelcomeCard from "@/components/WelcomeCard";
+import WelcomeCard from "@/components/Welcomecard";
 
 export default function HomeClient() {
   const [homeReady, setHomeReady] = useState(false);
