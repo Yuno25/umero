f50b336887c7ed22a8181b279f2829ebe39e4e9e
yuno <mshrivastava051@gmail.com>
2026-02-01 16:38:48 +0530

    Fix jsonwebtoken import for Next.js route handler

--- umero-landing/app/api/auth/me/route.ts
+++ umero-landing/app/api/auth/me/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
+import * jwt from "jsonwebtoken";
 import connectDB from "@/lib/db";
 import User from "@/models/User";
 
